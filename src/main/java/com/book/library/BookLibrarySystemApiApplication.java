a78614a365b1f14d1f8f4d64c7bbfb6e426a5e41
Nitesh <niteshprimedev@gmail.com>
2025-11-11 20:52:13 +0530

    feat: intial book api setup

---
+++ src/main/java/com/book/library/BookLibrarySystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.book.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibrarySystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibrarySystemApiApplication.class, args);
+    }
+
+}
